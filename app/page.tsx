b52a53da6ba00930d658d7115a71f055285a875a
Piston <piston@aegisos.cc>
2026-05-07 02:56:32 +0000

    feat: initial scaffold

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl">smoketest-1778122329</h1>
+    </main>
+  );
+}
